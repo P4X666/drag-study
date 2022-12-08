b9de008109d1334cc81479ff3162d0c5e7112346
pengshixing <2644378911@qq.com>
2022-12-08 22:47:57 +0800

    css

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from 'antd';
 import styles from './index.less';
 
 const Home = () => {
@@ -5,6 +6,7 @@ const Home = () => {
   return <div className={styles.index}>
     <h3>拖拽的简单展示</h3>
     <h3>与 antd 结合</h3>
+    <Button type="primary">Button</Button>
   </div>;
 };
 
