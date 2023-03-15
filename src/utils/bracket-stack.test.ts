126ad3f7435b0b8fa7a88b41db486c62708737ec
彭士兴 <2644378911@qq.com>
2023-03-15 15:46:57 +0800

    兼容emoji

--- src/utils/bracket-stack.test.ts
+++ src/utils/bracket-stack.test.ts
@@ -2,41 +2,51 @@ import { BracketStack } from './bracket-stack';
 
 describe('两个大括号的匹配', () => {
   it('空字符串', () => {
-    const bracket = new BracketStack('');
+    const bracket = new BracketStack([ '' ]);
     const strArr = bracket.getVariableArr();
     expect(strArr).toEqual([]);
   });
   it('没有括号的字符串', () => {
-    const bracket = new BracketStack('asfdgsdfgsdfgdsgsdtrhxcghdfhty');
+    const bracket = new BracketStack(Array.from('asfdgsdfgsdfgdsgsdtrhxcghdfhty'));
     const strArr = bracket.getVariableArr();
     expect(strArr).toEqual([]);
   });
   it('只有左括号', () => {
-    const bracket = new BracketStack('{{');
+    const bracket = new BracketStack(Array.from('{{'));
     const strArr = bracket.getVariableArr();
-    expect(strArr).toEqual([ ]);
+    expect(strArr).toEqual([]);
   });
   it('只有右括号', () => {
-    const bracket = new BracketStack('}}');
+    const bracket = new BracketStack(Array.from('}}'));
     const strArr = bracket.getVariableArr();
-    expect(strArr).toEqual([ ]);
+    expect(strArr).toEqual([]);
   });
   it('只有单个括号', () => {
-    const leftbracket = new BracketStack('{');
+    const leftbracket = new BracketStack(Array.from('{'));
     const leftStrArr = leftbracket.getVariableArr();
     expect(leftStrArr).toEqual([]);
-    const bracket = new BracketStack('}');
+    const bracket = new BracketStack(Array.from('}'));
     const strArr = bracket.getVariableArr();
-    expect(strArr).toEqual([ ]);
+    expect(strArr).toEqual([]);
   });
   it('匹配到一个', () => {
-    const bracket = new BracketStack('{{666}}');
+    const bracket = new BracketStack(Array.from('{{666}}'));
     const strArr = bracket.getVariableArr();
     expect(strArr).toEqual([ '666' ]);
   });
+  it('匹配到两个', () => {
+    const bracket = new BracketStack(Array.from('{{666}}{{666}}'));
+    const strArr = bracket.getVariableArr();
+    expect(strArr).toEqual([ '666', '666' ]);
+  });
   it('匹配到一个内嵌一个', () => {
-    const bracket = new BracketStack('{{6{{6}}6}}');
+    const bracket = new BracketStack(Array.from('{{6{{6}}6}}'));
     const strArr = bracket.getVariableArr();
     expect(strArr).toEqual([ '6' ]);
+  });
+  it('字符串中有emoji', () => {
+    const bracket = new BracketStack(Array.from('😘{{66😜6}}😁{{666}}😂'));
+    const strArr = bracket.getVariableArr();
+    expect(strArr).toEqual([ '66😜6', '666' ]);
   });
 });
